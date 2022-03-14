c04279a5e423e6157dfc0f4c7b606925bbe20226
Pranav <naringrekarpranav6@live.com>
2022-03-15 02:13:36 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const csp = require('express-csp');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //  GLOBAL MIDDLE WARES->
+//  Implementing CORS (Access-Control-Allow-Origin header)
+app.use(cors());
+app.options('*', cors());
+
 //  Serving static files
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
