d89f7f70394c9c196181698f0e2f737297aa64c4
Pranav <naringrekarpranav6@live.com>
2022-03-15 05:06:23 +0530

    Fixing bugs

--- public/js/index.js
+++ public/js/index.js
@@ -116,4 +116,4 @@ if (bookBtn)
 //   });
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alert) showAlert('success', alertMessage, 15);
+if (alertMessage) showAlert('success', alertMessage, 15);
