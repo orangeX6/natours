c584b95598cb1cb850ff80861529c75e105be0f5
Pranav <naringrekarpranav6@live.com>
2022-03-15 04:58:47 +0530

    Fixing bugs

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,7 +4,7 @@ const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
-exports.alert = (req, res, next) => {
+exports.alerts = (req, res, next) => {
   const { alert } = req.query;
   if (alert === 'booking')
     res.locals.alert = `Your booking was successful! Please check your email for a confirmation. If your booking does'nt show up here immediately, please come back later.`;
