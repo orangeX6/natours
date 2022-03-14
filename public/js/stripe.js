86b2dad14cc4f548eb54e1d30917f5361301465a
Pranav <naringrekarpranav6@live.com>
2022-03-15 04:30:42 +0530

    Improved Stripe Implementation

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -13,11 +13,11 @@ export const bookTour = async (tourId) => {
     // console.log(session);
 
     //  2) Create checkout form + charge credit card
-    await stripe.redirectToCheckout({
-      sessionId: session.data.session.id,
-    });
+    // await stripe.redirectToCheckout({
+    //   sessionId: session.data.session.id,
+    // });
 
-    // window.location.replace(session.data.session.url);
+    window.location.replace(session.data.session.url);
   } catch (err) {
     console.log(err);
     showAlert('error', err);
