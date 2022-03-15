995933784da2c0d56275f38af08d04dbe2df14ad
Pranav <naringrekarpranav6@live.com>
2022-03-15 05:42:56 +0530

    Fixing bugs

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
+const stripe = require('stripe');
 
 export const bookTour = async (tourId) => {
   try {
@@ -13,11 +14,11 @@ export const bookTour = async (tourId) => {
     // console.log(session);
 
     //  2) Create checkout form + charge credit card
-    await stripe.redirectToCheckout({
-      sessionId: session.data.session.id,
-    });
+    // await stripe.redirectToCheckout({
+    //   sessionId: session.data.session.id,
+    // });
 
-    // window.location.replace(session.data.session.url);
+    window.location.replace(session.data.session.url);
   } catch (err) {
     console.log(err);
     showAlert('error', err);
