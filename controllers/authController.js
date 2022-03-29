12dd0a6d5b93d1f81805e86f581d579c4106ccd0
Pranav <naringrekarpranav6@live.com>
2022-03-29 16:51:15 +0530

    Added Dev changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,6 +22,15 @@ const createSendToken = (user, statusCode, req, res) => {
     secure: req.secure || req.headers('x-forwarded-proto') === 'https',
   });
 
+  //  DEV
+  // const cookieOptions = {
+  //   expires: new Date(
+  //     Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
+  //   ),
+  //   httpOnly: true,
+  // };
+  // res.cookie('jwt', token, cookieOptions);
+
   //  Remove password from output
   user.password = undefined;
 
